b35b1c7c1e843cf190ead810e4d2fb4a17971bde
hugohu0224 <hugohu0224@gmail.com>
2024-09-03 06:57:33 +0800

    Refactor main.go to improve the error handling and graceful shutdown

--- cmd/producer.go
+++ cmd/producer.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 	"wikitracker/internal"
 )
@@ -13,16 +17,42 @@ func main() {
 
 	url := "https://stream.wikimedia.org/v2/stream/recentchange"
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	errChan := make(chan error, 1)
+	go func() {
+		errChan <- internal.Streaming(ctx, producer, url)
+	}()
+
 	for {
-		err := internal.Streaming(producer, url)
-		if err != nil {
-			if err == io.EOF {
-				fmt.Println("Connection closed. Retrying in 5 seconds...")
-				time.Sleep(5 * time.Second)
-				continue
+		select {
+		case err := <-errChan:
+			if err != nil {
+				if err == io.EOF {
+					fmt.Println("connection closed. retrying in 5 seconds...")
+					time.Sleep(5 * time.Second)
+					go func() {
+						errChan <- internal.Streaming(ctx, producer, url)
+					}()
+				} else {
+					fmt.Println("error processing stream:", err)
+					time.Sleep(5 * time.Second)
+					go func() {
+						errChan <- internal.Streaming(ctx, producer, url)
+					}()
+				}
 			}
-			fmt.Println("Error processing stream:", err)
-			time.Sleep(5 * time.Second)
+		case sig := <-signalChan:
+			fmt.Printf("received signal: %v. Shutting down...\n", sig)
+			cancel()
+			return
+		case <-ctx.Done():
+			fmt.Println("context canceled, shutting down...")
+			return
 		}
 	}
 }
